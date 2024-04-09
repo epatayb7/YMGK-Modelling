cd0a34696dc43974af90861273aa59aa441564d0
Bünyamin Epatay <74611969+epatayb7@users.noreply.github.com>
2024-04-09 20:31:42 +0300

    Update baker_map.py

--- baker_map.py
+++ baker_map.py
@@ -1,22 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def baker_map(x, y):
     if x < 0.5:
         return 2 * x, y / 2
     else:
         return 2 * (1 - x), 1 - (y / 2)
 
+np.random.seed(0) 
+points = np.random.rand(1000, 2)
 
-# Noktaların başlangıç koordinatlarını rastgele oluştur
-np.random.seed(0)  # Sonuçların tutarlı olması için
-points = np.random.rand(1000, 2)  # 1000 rastgele nokta
-
-# Baker Haritası uygulama
 mapped_points = np.array([baker_map(x, y) for x, y in points])
 
-# Sonuçları görselleştirme
 plt.figure(figsize=(10, 5))
 
 plt.subplot(1, 2, 1)
